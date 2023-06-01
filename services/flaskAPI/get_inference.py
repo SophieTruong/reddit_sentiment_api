8afe305add3d83ea01738c9efd7812d69a95b23c
SophieTruong <sophietruong1992@gmail.com>
2023-06-01 11:52:47 +0300

    Add dvc model tracking

--- services/flaskAPI/get_inference.py
+++ services/flaskAPI/get_inference.py
@@ -2,17 +2,26 @@
 from typing import List
 import pickle
 import pathlib
+import dvc.api
+
+data = dvc.api.read(
+    'setfit-reddit-suomi-2022-2023.pkl.dvc',
+    repo='https://github.com/SophieTruong/reddit_sentiment_api.git',
+    remote='azure',
+    mode='rb'
+)
+model = pickle.loads(data)
 
 # current working directory
 print(str(pathlib.Path().absolute()))
-sf = pickle.load(open('setfit-reddit-suomi-2022-2023.pickle', 'rb'))
+# sf = pickle.load(open('setfit-reddit-suomi-2022-2023.pickle', 'rb'))
 
 
 def get_inference(data: List[str]):
     '''
     call model prediction
     '''
-    preds = sf(data)
+    preds = model(data)
     return preds.tolist()
 
 
