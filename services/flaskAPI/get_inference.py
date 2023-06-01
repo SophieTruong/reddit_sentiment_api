504dcf9cdfcfd7558ac73bc8d186b762909b814a
SophieTruong <sophietruong1992@gmail.com>
2023-06-01 16:00:46 +0300

    remove dvc

--- services/flaskAPI/get_inference.py
+++ services/flaskAPI/get_inference.py
@@ -2,26 +2,38 @@
 from typing import List
 import pickle
 import pathlib
-import dvc.api
+from azure.storage.blob import BlobServiceClient
+import time 
+import os
+from dotenv import load_dotenv
 
-data = dvc.api.read(
-    'setfit-reddit-suomi-2022-2023.pkl.dvc',
-    repo='https://github.com/SophieTruong/reddit_sentiment_api.git',
-    remote='azure',
-    mode='rb'
-)
-model = pickle.loads(data)
+load_dotenv()
 
-# current working directory
-print(str(pathlib.Path().absolute()))
-# sf = pickle.load(open('setfit-reddit-suomi-2022-2023.pickle', 'rb'))
+STORAGEACCOUNTURL= os.getenv('STORAGEACCOUNTURL')
+STORAGEACCOUNTKEY=  os.getenv('STORAGEACCOUNTKEY')
+LOCALFILENAME=  os.getenv('LOCALFILENAME')
+CONTAINERNAME=  os.getenv('CONTAINERNAME')
+BLOBNAME=  os.getenv('BLOBNAME')
+
+#download from blob
+t1=time.time()
+blob_service_client_instance = BlobServiceClient(account_url=STORAGEACCOUNTURL, AzureSasCredential=STORAGEACCOUNTKEY)
+blob_client_instance = blob_service_client_instance.get_blob_client(CONTAINERNAME, BLOBNAME, snapshot=None)
+with open(LOCALFILENAME, "wb") as my_blob:
+    blob_data = blob_client_instance.download_blob()
+    blob_data.readinto(my_blob)
+t2=time.time()
+print(("It takes %s seconds to download "+BLOBNAME) % (t2 - t1))
+
+
+sf = pickle.load(open('setfit-reddit-suomi-2022-2023.pickle', 'rb'))
 
 
 def get_inference(data: List[str]):
     '''
     call model prediction
     '''
-    preds = model(data)
+    preds = sf(data)
     return preds.tolist()
 
 
